8ad63aa666f5542723638d3ce200d4e6599a148b
Pascal Henry Bauer <pascal.bauer@rwth-aachen.de>
2023-02-24 17:01:22 +0100

    added badly formatted file

---
+++ formatting_example.cpp
@@ -0,0 +1,40 @@
+#include "b.hpp"
+#include "z.hpp"
+#include "a.hpp"
+
+class formatting_example
+{
+private:
+ float state;
+public:
+formatting_example();
+    virtual
+    ~formatting_example();
+    
+    // Breaking strings
+    static constexpr NAME = "veryveryveryveryveryveryveryveryveryveryveryvery long name";
+    
+    static
+    struct structure
+    {
+        int state;
+    };
+    
+    };
+
+formatting_example::formatting_example()
+{
+    // Automated intendation and linebreaking
+        if (error)
+     {panic();
+     
+    }else if (some_condition || very_long_function(aligator,cargo,  airplane, std::make_unique<struct s>({1,2,3,4,5,6})))
+        {
+        a=b+c;struct s{int real, int cmplx};
+     }else 
+    goto spaghetti;
+    
+    return CONST;
+}
+
+
